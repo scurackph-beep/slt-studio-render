9a5c059e17b5948bb4c532d7e0946fa2484811e7
scurackph-beep <scurackph-beep@users.noreply.github.com>
2026-07-10 19:04:54 -0400

    Fuerza fondo negro

--- src/App.jsx
+++ src/App.jsx
@@ -12,21 +12,24 @@ import { StudioProvider } from './context/StudioContext';
 
 export default function App() {
   return (
-    <StudioProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<Layout />}>
-          <Route path="/video" element={<VideoStudio />} />
-          <Route path="/music" element={<MusicStudio />} />
-          <Route path="/sound" element={<SoundStudio />} />
-          <Route path="/image" element={<ImageStudio />} />
-          <Route path="/fashion" element={<FashionStudio />} />
-          <Route path="/engineering" element={<EngineeringLab />} />
-          <Route path="/ceo" element={<CEODashboard />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </StudioProvider>
+    /* FORZAMOS EL FONDO NEGRO DESDE LA RAÍZ POR SI EL CSS ESTÁ MUERTO */
+    <div style={{ backgroundColor: '#050505', minHeight: '100vh', width: '100%', color: '#ffffff' }}>
+      <StudioProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route element={<Layout />}>
+              <Route path="/video" element={<VideoStudio />} />
+              <Route path="/music" element={<MusicStudio />} />
+              <Route path="/sound" element={<SoundStudio />} />
+              <Route path="/image" element={<ImageStudio />} />
+              <Route path="/fashion" element={<FashionStudio />} />
+              <Route path="/engineering" element={<EngineeringLab />} />
+              <Route path="/ceo" element={<CEODashboard />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </StudioProvider>
+    </div>
   );
 }
